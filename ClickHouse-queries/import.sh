6ac72f71e1243e2a6729c981245f091515135ef0
XinShuoWang <56379080+XinShuoWang@users.noreply.github.com>
2021-11-08 20:48:08 +0800

    Update import.sh

--- ClickHouse-queries/import.sh
+++ ClickHouse-queries/import.sh
@@ -1,4 +1,4 @@
-./clickhouse-client --query "INSERT INTO customer FORMAT CSV" < customer.tbl
-./clickhouse-client --query "INSERT INTO part FORMAT CSV" < part.tbl
-./clickhouse-client --query "INSERT INTO supplier FORMAT CSV" < supplier.tbl
-./clickhouse-client --query "INSERT INTO lineorder FORMAT CSV" < lineorder.tbl
+./clickhouse-client --query "INSERT INTO customer FORMAT CSV" < /home/ubuntu/ssb-dbgen/customer.tbl
+./clickhouse-client --query "INSERT INTO part FORMAT CSV" < /home/ubuntu/ssb-dbgen/part.tbl
+./clickhouse-client --query "INSERT INTO supplier FORMAT CSV" < /home/ubuntu/ssb-dbgen/supplier.tbl
+./clickhouse-client --query "INSERT INTO lineorder FORMAT CSV" < /home/ubuntu/ssb-dbgen/lineorder.tbl
