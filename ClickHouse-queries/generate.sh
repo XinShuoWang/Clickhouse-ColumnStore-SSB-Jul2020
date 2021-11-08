ce754742df97d6a82deed24f53e788fd5367c577
XinShuoWang <56379080+XinShuoWang@users.noreply.github.com>
2021-11-08 20:46:14 +0800

    Update generate.sh

--- ClickHouse-queries/generate.sh
+++ ClickHouse-queries/generate.sh
@@ -1,5 +1,5 @@
-./dbgen -s 1000 -T c
-./dbgen -s 1000 -T l
-./dbgen -s 1000 -T p
-./dbgen -s 1000 -T s
-./dbgen -s 1000 -T d
+./dbgen -s 1 -T c
+./dbgen -s 1 -T l
+./dbgen -s 1 -T p
+./dbgen -s 1 -T s
+./dbgen -s 1 -T d
