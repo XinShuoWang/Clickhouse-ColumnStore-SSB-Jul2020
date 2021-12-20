f1cca5e7cb896aac1d3b9a1f754072a92451af06
XinShuoWang <56379080+XinShuoWang@users.noreply.github.com>
2021-12-20 19:48:52 +0800

    Update clickhouse-schema.sql

--- clickhouse-schema.sql
+++ clickhouse-schema.sql
@@ -11,27 +11,7 @@ CREATE TABLE customer
 )
 ENGINE = MergeTree ORDER BY (C_CUSTKEY);
 
-CREATE TABLE lineorder
-(
-    LO_ORDERKEY             UInt32,
-    LO_LINENUMBER           UInt8,
-    LO_CUSTKEY              UInt32,
-    LO_PARTKEY              UInt32,
-    LO_SUPPKEY              UInt32,
-    LO_ORDERDATE            Date,
-    LO_ORDERPRIORITY        LowCardinality(String),
-    LO_SHIPPRIORITY         UInt8,
-    LO_QUANTITY             UInt8,
-    LO_EXTENDEDPRICE        UInt32,
-    LO_ORDTOTALPRICE        UInt32,
-    LO_DISCOUNT             UInt8,
-    LO_REVENUE              UInt32,
-    LO_SUPPLYCOST           UInt32,
-    LO_TAX                  UInt8,
-    LO_COMMITDATE           Date,
-    LO_SHIPMODE             LowCardinality(String)
-)
-ENGINE = MergeTree PARTITION BY toYear(LO_ORDERDATE) ORDER BY (LO_ORDERDATE, LO_ORDERKEY);
+CREATE TABLE lineorder(    LO_ORDERKEY             UInt32,    LO_LINENUMBER           UInt8,    LO_CUSTKEY              UInt32,    LO_PARTKEY              UInt32,    LO_SUPPKEY              UInt32,    LO_ORDERDATE            Date,    LO_ORDERPRIORITY        LowCardinality(String),    LO_SHIPPRIORITY         UInt8,    LO_QUANTITY             UInt8,    LO_EXTENDEDPRICE        UInt32,    LO_ORDTOTALPRICE        UInt32,    LO_DISCOUNT             UInt8,    LO_REVENUE              UInt32,    LO_SUPPLYCOST           UInt32,    LO_TAX                  UInt8,    LO_COMMITDATE           Date,    LO_SHIPMODE             LowCardinality(String))ENGINE = MergeTree PARTITION BY toYear(LO_ORDERDATE) ORDER BY (LO_ORDERDATE, LO_ORDERKEY);
 
 CREATE TABLE part
 (
